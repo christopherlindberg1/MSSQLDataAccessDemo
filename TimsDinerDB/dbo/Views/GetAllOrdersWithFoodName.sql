6941bdf3dd68b9ac672fd3b82c822e2e9e119e16
christopherlindberg1 <christopher.lindberg1@gmail.com>
2021-02-11 14:14:32 +0100

    View for getting food information for each order

---
+++ TimsDinerDB/dbo/Views/GetAllOrdersWithFoodName.sql
@@ -0,0 +1,7 @@
+﻿CREATE VIEW [dbo].[GetAllOrdersWithFoodName]
+	AS SELECT
+	[O].[Id], [O].[OrderName], [O].[OrderDate], [O].[Quantity], [O].[Total],
+	[F].[Title], [F].[Description], [F].[Price]
+	FROM dbo.[Order] AS O
+	INNER JOIN dbo.[Food] AS F
+	ON O.FoodId = F.Id;

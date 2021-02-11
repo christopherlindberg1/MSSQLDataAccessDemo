997d3bc73aeacd127b14df3eb14f2887d9cab33f
christopherlindberg1 <christopher.lindberg1@gmail.com>
2021-02-11 16:47:21 +0100

    Modified column names to match the model

--- TimsDinerDB/dbo/Views/GetAllOrdersWithFoodName.sql
+++ TimsDinerDB/dbo/Views/GetAllOrdersWithFoodName.sql
@@ -1,7 +1,7 @@
 ﻿CREATE VIEW [dbo].[GetAllOrdersWithFoodName]
 	AS SELECT
 	[O].[Id], [O].[OrderName], [O].[OrderDate], [O].[Quantity], [O].[Total],
-	[F].[Title], [F].[Description], [F].[Price]
+	[F].[Title] AS [FoodTitle], [F].[Description] AS [FoodDescription], [F].[Price] AS [FoodPrice]
 	FROM dbo.[Order] AS O
 	INNER JOIN dbo.[Food] AS F
 	ON O.FoodId = F.Id;
